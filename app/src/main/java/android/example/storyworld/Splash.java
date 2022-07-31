f618e7b66f8c6a312d0add1339f46f8c182101a8
Deepak <dk8400779@gmail.com>
2022-08-01 00:26:16 +0530

    Initial Commit

--- app/src/main/java/android/example/storyworld/Splash.java
+++ app/src/main/java/android/example/storyworld/Splash.java
@@ -21,7 +21,7 @@ public class Splash extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(getApplicationContext(), Story.class);
+                Intent intent = new Intent(getApplicationContext(), Stories.class);
                 startActivity(intent);
                 finish();
             }
